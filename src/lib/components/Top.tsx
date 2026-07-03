e4405929e0f1965cd27132d2d7e10e4fd78a842f
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-03 14:47:25 +0500

    top

--- src/lib/components/Top.tsx
+++ src/lib/components/Top.tsx
@@ -1,17 +1,23 @@
-import { Button } from "antd";
+import { Button, Input } from "antd";
+
 
 const Top = () => {
   return (
-    <div className=" flex justify-between items-center">
-      <div>
-        <p>Мини CRUD</p>
-        <p>Поиск • Модальные окна • Карточки</p>
+    <>
+      <div className=" flex justify-between items-center">
+        <div>
+          <p className=" text-4xl font-bold">Мини CRUD</p>
+          <p className=" text-white/60 pt-2">
+            Поиск • Модальные окна • Карточки
+          </p>
+        </div>
+        <div className=" flex justify-center items-center gap-7">
+          <p className="text-white/60">Всего: 0</p>
+          <Button>+ Добавить</Button>
+        </div>
       </div>
-      <div>
-        <p>Всего: 0</p>
-        <Button>+ Добавить</Button>
-      </div>
-    </div>
+      <Input placeholder="Поиск..."  variant="underlined" className="bg-white/0! w-120! text-white! placeholder:text-white/80! mt-15!"/>
+    </>
   );
 };
 
