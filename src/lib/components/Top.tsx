65ba8d38967669852cd996981c6ccaafecd22a0f
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-03 14:30:52 +0500

    update

--- src/lib/components/Top.tsx
+++ src/lib/components/Top.tsx
@@ -2,8 +2,14 @@
 
 const Top = () => {
   return (
-    <div>Top</div>
-  )
+    <div>
+      <div>
+        <p>Мини CRUD</p>
+        <p>Поиск • Модальные окна • Карточки</p>
+      </div>
+      <div><p>Всего: 0</p></div>
+    </div>
+  );
 }
 
 export default Top
