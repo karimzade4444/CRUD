2ee4bbebf3def01e87fe66b14bb6798aaec32edb
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-03 14:57:39 +0500

    img

--- src/lib/components/Bottom.tsx
+++ src/lib/components/Bottom.tsx
@@ -1,8 +1,21 @@
+import { useQuery } from "@tanstack/react-query"
+import { getUsers } from "../api/api"
 
 
 const Bottom = () => {
+    const { data } = useQuery({
+        queryFn:getUsers,
+        queryKey:["getUsers"]
+    });
+
   return (
-    <div>Bottom</div>
+    <div className=" flex justify-around flex-wrap gap-10 mt-10">
+     {data?.map((el)=>(
+        <div key={el.id} className="w-100 h-80 bg-white rounded-2xl">
+           
+        </div>
+     ))}
+    </div>
   )
 }
 
