e61434dd714f973bf2d93a4a2f441126d2c04e95
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-03 14:50:36 +0500

    data

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,7 +1,11 @@
+import axios from "axios";
+import type { IGetUsers } from "../types/types";
 
 
 
 const api = "https://69e5ff70ce4e908a155ec5a1.mockapi.io/users";
 
 
-export const getUsers = async ():
+export const getUsers = async ():Promise<IGetUsers[]>=>{
+    return (await axios.get(api)).data;
+};
